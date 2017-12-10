df678aa4a65d258fd66506a65f79b76abec032ba
Matthew Levan <matt.s.levan@gmail.com>
2017-12-09 19:38:14 -0800

    Add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import SignIn from '@/components/SignIn';
+import Admin from '@/components/Admin';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'SignIn',
+      component: SignIn,
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin,
     },
   ],
 });
